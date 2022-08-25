1bfee2ecfa0e2ffb63180d547ba740b5051a29ed
Ankita Singh <anki3404@gmail.com>
2022-08-25 15:05:04 +0530

    code push

--- demo/src/app/electronics/electronics.component.ts
+++ demo/src/app/electronics/electronics.component.ts
@@ -13,11 +13,60 @@ export class ElectronicsComponent implements OnInit {
   ngOnInit(): void {
     this.model=[
       {
-        "src":"hgj",
-        "desc":"fgfdg",
-        "amount":"4.5",
-        "status":"ghj",
-        "rating":"56"
+        "src":"	https://ecommerce-angular-demo.setproduct.com/assets/images/img3.jpg",
+        "desc":"One string Bonsai description",
+        "amount":"$11.68",
+        "status":"Worldwide shifting available Buyers protection possible",
+        "rating":"4.99"
+      },
+      {
+        "src":"	https://ecommerce-angular-demo.setproduct.com/assets/images/img9.jpg",
+        "desc":"Plastic useless plugs and tubes for high-fidelity prototyping. Fit & Eat",
+        "amount":"$12.48",
+        "status":"Worldwide shifting available Buyers protection possible",
+        "rating":"4.99"
+      },
+      {
+        "src":"	https://ecommerce-angular-demo.setproduct.com/assets/images/img12.jpg",
+        "desc":"Vintage Typewriter to post awesome stories about UI design and web development",
+        "amount":"$49.50",
+        "status":"Eligible for shipping to mars or somewhere else",
+        "rating":"4.05"
+      },
+      {
+        "src":"https://ecommerce-angular-demo.setproduct.com/assets/images/img11.jpg",
+        "desc":"Lee Pucker design. Leather botinki for handsome designers. Free shipping",
+        "amount":"$13.95",
+        "status":"1258 bids, 359 watches $5.95 for shipping",
+        "rating":"4.56"
+      },
+      {
+        "src":"https://ecommerce-angular-demo.setproduct.com/assets/images/img8.jpg",
+        "desc":"Vintage Typewriter to post awesome stories about UI design and web development",
+        "amount":"$49.50",
+        "status":"Eligible for shipping to mars or somewhere else",
+        "rating":"4.05"
+      },
+      {
+        "src":"https://ecommerce-angular-demo.setproduct.com/assets/images/img7.jpg",
+        "desc":"Lee Pucker design. Leather botinki for handsome designers. Free shipping",
+        "amount":"$13.95",
+        "status":"1258 bids, 359 watches $5.95 for shipping",
+        "rating":"4.56"
+      },
+      {
+        "src":"https://ecommerce-angular-demo.setproduct.com/assets/images/img8.jpg",
+        "desc":"Vintage Typewriter to post awesome stories about UI design and web development",
+        "amount":"$49.50",
+        "status":"Eligible for shipping to mars or somewhere else",
+        "rating":"4.05"
+      },
+      {
+        "src":"https://ecommerce-angular-demo.setproduct.com/assets/images/img7.jpg",
+        "desc":"Lee Pucker design. Leather botinki for handsome designers. Free shipping",
+        "amount":"$13.95",
+        "status":"1258 bids, 359 watches $5.95 for shipping",
+        "rating":"4.56"
       }
     ]
   }
